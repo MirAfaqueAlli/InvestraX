1e554410442043b70d34ea76ccd55e55e8d3d397
Mir Afaque Alli <70464567+MirAfaqueAlli@users.noreply.github.com>
2025-12-27 11:29:04 +0530

    Update Home.jsx

--- dashboard/src/components/Home.jsx
+++ dashboard/src/components/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 import Dashboard from "./Dashboard";
-import TopBar from "./TopBar";
+import TopBar from "./Topbar";
 
 const Home = () => {
   return (
     <>
-      <TopBar />
+      <Topbar />
       <Dashboard />
     </>
   );
